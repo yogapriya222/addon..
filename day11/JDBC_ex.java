79aab5d03a0c13378ebcdee2707174383e783bcf
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-19 10:41:11 +0530

    task completed

--- day11/JDBC_ex.java
+++ day11/JDBC_ex.java
@@ -21,33 +21,6 @@ public static void main(String[] args) throws ClassNotFoundException, SQLExcepti
 	String query="create table stu(id int,name varchar(20),age int)";
 	stmt.executeUpdate(query);
 	System.out.println("table created");
-//	String query="select * from stu";
-//	ResultSet rs=stmt.executeQuery(query);
-//	while(rs.next()) {
-//
-//		System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));
-//	}
-//	while(rs.next())
-//	{
-//		System.out.println(rs.getInt("id")+" "+rs.getString("name")+" "+rs.getInt("age"));
-//	}
-//	Scanner sc=new Scanner(System.in);
-//	System.out.println("enter your id");
-//	int id=sc.nextInt();
-//	sc.nextLine();
-//	System.out.println("enter your name");
-//	String name=sc.nextLine();
-//	System.out.println("enter your age");
-//	int age=sc.nextInt();
-//	
-//	
-//	String query="insert into stu values(?,?,?)";
-//	PreparedStatement ps=con.prepareStatement(query);
-//
-//	ps.setInt(1, id);
-//	ps.setString(2, name);
-//	ps.setInt(3, age);
-//ps.execute();
-//System.out.println("execute successfully");
+
 }
 }
